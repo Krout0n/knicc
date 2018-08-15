1b8fc5dd37b9db76da771da4e3dee7c0a316e52b
Knium <hi.im.knium@gmail.com>
2018-08-15 17:16:22 +0900

    use main() when testing

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ make compiler
 exit_code() {
   expr=$1
   expected=$2
-  echo "main(){" $expr "}" | ./compiler > test.s
+  echo $expr | ./compiler > test.s
   gcc test.s
   ./a.out
   exit_code=$?
@@ -30,39 +30,47 @@ printing_test() {
   rm -rf a.out
 }
 
-exit_code "1;" 1
-exit_code "10;" 10
-exit_code "22;" 22
-exit_code '1+2;' 3
-exit_code '12+34;' 46
-exit_code '1+2+3;' 6
-exit_code '11+22+33;' 66
-exit_code '3-2;' 1
-exit_code '3-2+1;' 2
-exit_code '40-20+11;' 31
-exit_code '99-21-37;' 41
-exit_code '1*2;' 2
-exit_code '1*2*3;' 6
-exit_code '1+2*3;' 7
-exit_code '1*2+3*4;' 14
-exit_code '1+2*3+4;' 11
-exit_code '15-3*4;' 3
-exit_code '1+2; 3+4;' 7
-exit_code '1+2; 1*4+3*10;' 34
-exit_code '11+22; 33+44; 114-10-40*2;' 24
-exit_code '(1+2)*3;' 9
-exit_code '1+(2*3);' 7
-exit_code '(1*2)+3;' 5
-exit_code '(1+2+3);' 6
-exit_code '(9-5)*10;' 40
-# exit_code 'a=1;' 1
-# exit_code 'a=1+2;' 3
-# exit_code 'a=1;b=2;' 2
-# exit_code 'a=1+2+3;10+20;' 30
+exit_code "main(){1;}" 1
+exit_code "main(){10;}" 10
+exit_code "main(){22;}" 22
+exit_code 'main(){1+2;}' 3
+exit_code 'main(){12+34;}' 46
+exit_code 'main(){1+2+3;}' 6
+exit_code 'main(){11+22+33;}' 66
+exit_code 'main(){3-2;}' 1
+exit_code 'main(){3-2+1;}' 2
+exit_code 'main(){40-20+11;}' 31
+exit_code 'main(){99-21-37;}' 41
+exit_code 'main(){1*2;}' 2
+exit_code 'main(){1*2*3;}' 6
+exit_code 'main(){1+2*3;}' 7
+exit_code 'main(){1*2+3*4;}' 14
+exit_code 'main(){1+2*3+4;}' 11
+exit_code 'main(){15-3*4;}' 3
+exit_code 'main(){1+2; 3+4;}' 7
+exit_code 'main(){1+2; 1*4+3*10;}' 34
+exit_code 'main(){11+22; 33+44; 114-10-40*2;}' 24
+exit_code 'main(){(1+2)*3;}' 9
+exit_code 'main(){1+(2*3);}' 7
+exit_code 'main(){(1*2)+3;}' 5
+exit_code 'main(){(1+2+3);}' 6
+exit_code 'main(){(9-5)*10;}' 40
+
+exit_code 'main() { a=1; a;}' 1
+exit_code 'main() { a=1+2; a;}' 3
+exit_code 'main() { a=1;b=2; b;}' 2
+exit_code 'main() { a=1+2+3;10+20;}' 30
+exit_code 'main() { a=3; a;}' '3'
+exit_code 'main() { a=1; b=2; a+b;}' '3'
+
 printing_test 'print_ok();' "OK"
 printing_test '1+2; print_ok();' "OK"
 printing_test 'add_one(1);' "2"
 printing_test 'add(1,2);' "3"
 printing_test 'print_all_args(1,2,3,4,5,6);' "123456"
+
+exit_code 'foo(x){ x + 1; } main(){ foo(1); }' "2"
+exit_code 'add(x,y) { x + y; } main() { add(1,2); }' '3'
+exit_code 'add(x,y) { x + y; } main() { a=1; b=2; add(a,b);' '3'
 
 make clean
